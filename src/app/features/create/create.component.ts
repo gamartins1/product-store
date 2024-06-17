d0abdea936883639b36006ddd26a112fe0efbffc
Gabriel Martins <gabriel.550@hotmail.com>
2024-06-17 19:45:25 -0300

    feat: Snackbar com informação de produto criado/roteamento para a home após criar o produto

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ProductsService } from '../../shared/services/products.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -15,6 +17,9 @@ import { ProductsService } from '../../shared/services/products.service';
 export class CreateComponent {
   productsService = inject(ProductsService);
 
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
+
   form = new FormGroup({
     title: new FormControl<string>('',
       {
@@ -24,10 +29,15 @@ export class CreateComponent {
   });
 
   onSubmit() {
+    
     this.productsService.post({
       title: this.form.controls.title.value
     }).subscribe(() => {
-      alert('Produto salvo com sucesso');
+      this.matSnackBar.open('Produto criado com sucesso', 'Ok', {
+        duration: 3000, horizontalPosition: 'right', verticalPosition: 'top'
+      });
+
+      this.router.navigateByUrl('/');
     });
     
   }
