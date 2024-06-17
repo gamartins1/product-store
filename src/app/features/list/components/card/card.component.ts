a056178225445ba8e7381ece6aeacc679442f579
Gabriel Martins <gabriel.550@hotmail.com>
2024-06-17 20:18:07 -0300

    feat: Funcionalidade de direcionar pro formulário de edição de produtos

--- src/app/features/list/components/card/card.component.ts
+++ src/app/features/list/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, EventEmitter, computed, input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Product } from '../../../../shared/interfaces/product.interface';
@@ -15,4 +15,6 @@ export class CardComponent {
   product = input.required<Product>();
   productTitle = computed(() => this.product().title);
 
+  @Output() edit = new EventEmitter();
+  
 }
